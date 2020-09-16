a6bc258a8d207ff7a7bc00448e948652223ee5c1
Xiristian <christianhederson22@gmail.com>
2020-09-16 12:34:04 -0300

    feat: Atividade 02

---
+++ atividade02.py
@@ -0,0 +1,8 @@
+meusAmigos = {
+    "Vitinho": 16,
+    "Victor": 16,
+    "Gabriel": 18,
+    "Debora": 17,
+    "Camila": 16
+}
+print("Dicionário com nome e idade das pessoas com quem tenho mais afinidade:", meusAmigos)
