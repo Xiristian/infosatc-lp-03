609b83a8902d7cbbd6461bda313d2766791a903d
Xiristian <christianhederson22@gmail.com>
2020-09-16 12:34:48 -0300

    feat: Atividade 05

---
+++ atividade05.py
@@ -0,0 +1,15 @@
+filmes = {}
+
+filmes["Star Wars: Ataque dos Clones"] = {
+    "Darth Sidious": 2002
+    }
+
+filmes["Star Wars: O Retorno de Jedi"] = {
+    "Darth Vader": 1983
+    }
+
+filmes["Star Wars: O Despertar da Força"] = {
+    "DaKylo Ren": 2015
+    }
+
+print("Nome do filme, vilão e ano em que foi lançado:", filmes)
